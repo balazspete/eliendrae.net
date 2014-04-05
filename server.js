edf3c39162d84360e6f93214fd543c58cd237c1b
Balazs Pete <bazsip@gmail.com>
2014-04-05 05:34:35 +0100

    Move server to index

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-require("coffee-script");
-require("./app");
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello Node.js!\n');
+    if ('host' in req.headers) {
+        var host = req.headers['host'];
+        res.write('Vhost is ' + host.split(':')[0] + '\n');
+    } else {
+        res.write('No vhost specified\n');
+    }
+    res.end();
+}).listen(8080);
